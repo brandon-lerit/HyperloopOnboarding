ef4732d8245b6fd9aad71f72abd4bbf09e4defc3
brandon-lerit <148146640+brandon-lerit@users.noreply.github.com>
2023-11-30 05:39:33 +0000

    Added GUI

---
+++ utils/body.py
@@ -0,0 +1,132 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from widgets import *
+from constants import *
+import pyqtgraph as pg
+
+
+class Body(QWidget):
+    def __init__(self, *args, **kwargs):
+        super(Body, self).__init__()
+        self.width = args[0]
+        self.height = args[1]
+        hbox = QHBoxLayout(self)
+
+        sshFile = "utils/body.css"
+        with open(sshFile, "r") as fh:
+            qstr = str(fh.read())
+
+        home = QSplitter(Qt.Vertical)
+
+        home_footer = QSplitter(Qt.Horizontal)
+        bottom_left = QSplitter(Qt.Vertical)
+        vel_acc = QSplitter(Qt.Horizontal)
+
+        speedometer = Speedometer()
+        accelerometer = Accelerometer()
+        vel_acc.addWidget(speedometer)
+        vel_acc.addWidget(accelerometer)
+
+        speed = Speed()
+        bottom_left.addWidget(vel_acc)
+        bottom_left.addWidget(speed)
+
+        # temporary graph
+        self.temporary_graph = pg.PlotWidget()
+        hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
+        self.temporary_graph.resize(int(self.width), int(self.height / 4))
+
+        self.plot_buttons = PlotButtons(self.temporary_graph)
+
+        # Data for plots
+        self.x_data = [[], [], [], [], [], []]
+        self.y_data = [[], [], [], [], [], []]
+        self.current_plot_indices = [0, 0, 0, 0, 0, 0]
+        self.current_plot_values = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]  # x,y for each plot
+        self.currentPlotIndex = 0 
+        self.currentPlotName = "Accelerometer"
+
+        prox_sensors = ProximitySensor()
+        home_footer.addWidget(bottom_left)
+        home_footer.addWidget(prox_sensors)
+
+        vgraph = QSplitter(Qt.Horizontal)
+        vgraph.setSizes([2, 2])
+
+        home.addWidget(self.temporary_graph)
+        home.addWidget(self.plot_buttons)
+        home.addWidget(home_footer)
+        home.setSizes([350, 5, 45])
+
+        hbox.addWidget(home)
+        self.setLayout(hbox)
+        self.setStyleSheet(qstr)
+
+        self.timer = QTimer(self, timeout=self.update)
+        self.timer.start(1000)
+        self.x = 0
+        self.y = 0
+
+    def update(self):
+        current_plot = self.plot_buttons.getCurrentPlot()
+        pen = pg.mkPen(width=10)
+
+        if (self.plot_buttons.getRescaleAxesFlag()):
+            self.plot_buttons.setRescaleAxesFlag(False)
+            x_axes = self.plot_buttons.getXAxesLimits()
+            y_axes = self.plot_buttons.getYAxesLimits()
+            self.temporary_graph.setXRange(x_axes[0], x_axes[1])
+            self.temporary_graph.setYRange(y_axes[0], y_axes[1])
+
+        if (self.plot_buttons.getPlotResetFlag()):
+            # Reset the current plot
+            self.current_plot_values[current_plot][0] = 0
+            self.current_plot_values[current_plot][1] = 0
+            self.current_plot_indices[current_plot] = 0
+            self.x_data[current_plot] = [0]
+            self.y_data[current_plot] = [0]
+            self.plot_buttons.setPlotResetFlag(False)
+
+        elif (self.plot_buttons.getChangedPlot()):
+            # Reset the plot
+            
+            self.currentPlotIndex = self.plot_buttons.getCurrentPlot
+            self.currentPlotName = self.plot_buttons.getCurrentPlotName
+            
+            self.temporary_graph.clear()
+            # add another getter in plot_buttons to get index of new graph 
+            # add another getter to get the name of the chaged graph 
+            # add another getter to get the x and y axes (units) of the changed graph
+
+            # Plot all the data for the new plot
+            for i in range(len(self.x_data[current_plot])):
+                self.temporary_graph.plot([self.x_data[current_plot][i]], [self.y_data[current_plot][i]],
+                                          pen=pen, symbol='x', symbolSize=30)
+
+            # Reset the changed plot flag
+            self.plot_buttons.setChangedPlot(False)
+
+        else:
+            # Updated variables for the next data
+            for i in range(NUM_PLOTS):
+                # if statement for amount of time left
+                self.x_data[i].append(self.current_plot_values[i][0])
+                self.y_data[i].append(self.current_plot_values[i][1])
+                self.current_plot_values[i][0] += PLOT_INCREMENTS[i]
+                self.current_plot_values[i][1] += PLOT_INCREMENTS[i]
+
+            # Get the current index
+            currentIndex = self.current_plot_indices[current_plot]
+
+            # Plot the current data point
+            self.temporary_graph.plot([self.x_data[current_plot][currentIndex]], [self.y_data[current_plot][currentIndex]],
+                                      pen=pen, symbol='x', symbolSize=30)
+
+            # Update the indices for the next datapoint
+            self.current_plot_indices[0] += 1
+            self.current_plot_indices[1] += 1
+
+    def dimensions(self):
+        return self.width, self.height
---
+++ utils/header.py
@@ -0,0 +1,119 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from widgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import QObject, pyqtSignal
+import time
+from widgets import progressBar
+import constants as cons
+
+
+class Header(QWidget):
+    def __init__(self, w=1000, h=500, *args, **kwargs):
+        super(Header, self).__init__()
+        hbox = QHBoxLayout(self)
+        sshFile = "utils/header.css"
+        with open(sshFile, "r") as fh:
+            qstr = str(fh.read())
+        self.width = w
+        self.height = h
+        hbox.setContentsMargins(0, 0, 0, 0)
+
+        grid1 = QGridLayout(self)
+
+        quit = Quit(w, h)
+        grid1.addWidget(quit, 0, 0, alignment=Qt.AlignCenter)
+
+        timer = Timer(self.width, self.height)
+        grid1.addWidget(timer, 0, 2, alignment=Qt.AlignCenter)
+
+        help = HelpPopup(int(self.width), int(self.height))
+        grid1.addWidget(help, 0, 3, alignment=Qt.AlignCenter)
+
+        fsm = FSM()
+
+        emergency_button = EmergencyButton(fsm, self.width, self.height)
+        grid1.addWidget(emergency_button, 0, 4,
+                        alignment=Qt.AlignCenter)
+
+        grid2 = QGridLayout(self)
+        self.b1 = QPushButton("Home")
+        self.b1.clicked.connect(lambda: self.navbar(self.b1))
+        self.b1.resize(int(self.width / 5), int(self.height / 20))
+        self.b2 = QPushButton("Visualizer")
+        self.b2.clicked.connect(lambda: self.navbar(self.b2))
+        self.b2.resize(int(self.width / 5), int(self.height / 20))
+        self.b3 = QPushButton("Battery")
+        self.b3.clicked.connect(lambda: self.navbar(self.b3))
+        self.b3.resize(int(self.width / 5), int(self.height / 20))
+        self.b4 = QPushButton("Temperature")
+        self.b4.clicked.connect(lambda: self.navbar(self.b4))
+        self.b4.resize(int(self.width / 5), int(self.height / 20))
+        self.b5 = QPushButton("FSM")
+        self.b5.clicked.connect(lambda: self.navbar(self.b5))
+        self.b5.resize(int(self.width / 5), int(self.height / 20))
+        grid2.addWidget(self.b1, 0, 0)
+        grid2.addWidget(self.b2, 0, 1)
+        grid2.addWidget(self.b3, 0, 2)
+        grid2.addWidget(self.b4, 0, 3)
+        grid2.addWidget(self.b5, 0, 4)
+
+        vbox = QVBoxLayout(self)
+
+        vbox.addLayout(grid1)
+        vbox.addLayout(grid2)
+
+        hbox.addLayout(vbox)
+        self.setStyleSheet(qstr)
+
+        splitter4 = QSplitter(Qt.Horizontal)
+        self.pBarContainer = progressBar.ProgressBar()
+        splitter4.addWidget(self.pBarContainer.label)
+
+        # PROGRESS BAR
+        splitter4.addWidget(self.pBarContainer.pBar)
+        splitter4.setSizes([int(self.height / 30), int(self.height / 30)])
+
+
+
+
+
+        hyperloop = QPixmap('state_icons/logo.png')
+        hyperloop = hyperloop.scaled(200, 100)
+        label = QLabel()
+        label.setStyleSheet("border: 1px grey")
+        label.setFixedWidth(200)
+        label.setFixedHeight(100)
+        label.setPixmap(hyperloop)
+
+        splitter4.addWidget(label)           
+
+        hbox.addWidget(splitter4)
+
+
+
+
+
+
+        self.timer = QTimer(self, timeout=self.update)
+        self.timer.start(1000)
+
+        self.show()
+
+    def update(self):  # PROGRESS BAR
+        self.pBarContainer.pBar.setValue(self.pBarContainer.pBar.value()+5)
+
+        if int(self.pBarContainer.pBar.value()) < 50:
+            self.pBarContainer.pBar.setStyleSheet(cons.PBAR_LOW_PROGRESS)
+        elif int(self.pBarContainer.pBar.value()) > 50:
+            self.pBarContainer.pBar.setStyleSheet(cons.PBAR_MED_PROGRESS)
+        elif int(self.pBarContainer.pBar.value()) == 100:
+            self.pBarContainer.pBar.setStyleSheet(cons.PBAR_HIGH_PROGRESS)
+
+    def navbar(self, b):
+        buttons = [self.b1, self.b2, self.b3, self.b4, self.b5]
+        # only temperature page is not implemented
+        if buttons.index(b) == 3:
+            return 0
+        else:
+            return buttons.index(b)
